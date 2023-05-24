e374eb7877384fe99c4307fc93965a0178469cd0
famouscat <onismaa@gmail.com>
2023-05-24 19:05:52 +0800

    chore: remove unuse code

--- src/components/ContentView.tsx
+++ src/components/ContentView.tsx
@@ -1,13 +1,12 @@
-import { TocEntry } from "remark-mdx-toc";
 import { useAtom } from "jotai";
 import { currentPageComponentAtom } from "../state";
 
 export function ContentView() {
-  const [Page, setPage] = useAtom(currentPageComponentAtom);
+  const [Page] = useAtom(currentPageComponentAtom);
   return (
     <>
       <Page fallback={<div>loading page</div>}>
-        {({ default: Add, title, toc }) => <Add />}
+        {({ default: Add }) => <Add />}
       </Page>
     </>
   );
