802492a5f7dd13e33b8dc5678ffa7afc3645af28
famouscat <onismaa@gmail.com>
2023-05-26 18:42:20 +0800

    chore: add post page director

--- src/components/ContentView.tsx
+++ src/components/ContentView.tsx
@@ -1,7 +1,48 @@
 import { useAtom } from "jotai";
 import { Toolbar } from "@mui/material";
 import { currentPageComponentAtom } from "../state";
+import { Box, Button } from "@mui/joy";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { posts } from "../state/posts";
 
+function PageChanger() {
+  const [page, setPage] = useAtom(currentPageComponentAtom);
+
+  const nextPage = () => {
+    const curIndex = posts.findIndex((post) => post === page);
+    const newIndex = curIndex + 1;
+    if (newIndex >= posts.length) {
+      return;
+    }
+    setPage(posts[newIndex]);
+  };
+  const lastPage = () => {
+    const curIndex = posts.findIndex((post) => post === page);
+    const newIndex = curIndex - 1;
+    if (newIndex < 0) return;
+    setPage(posts[newIndex]);
+  };
+
+  return (
+    <Box
+      sx={(theme) => ({
+        padding: `${theme.spacing(1)} 0`,
+        display: "flex",
+        justifyContent: "space-between",
+        position: "sticky",
+        bottom: 0,
+      })}
+    >
+      <Button onClick={lastPage} startDecorator={<KeyboardArrowLeftIcon />}>
+        Last Post
+      </Button>
+      <Button onClick={nextPage} endDecorator={<KeyboardArrowRightIcon />}>
+        Next Post
+      </Button>
+    </Box>
+  );
+}
 export function ContentView() {
   const [{ page: Page }] = useAtom(currentPageComponentAtom);
   return (
@@ -10,6 +51,7 @@ export function ContentView() {
       <Page fallback={<div>loading page</div>}>
         {({ default: Add }) => <Add />}
       </Page>
+      <PageChanger />
       <Toolbar />
     </>
   );
